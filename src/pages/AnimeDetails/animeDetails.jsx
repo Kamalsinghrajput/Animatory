048938ce5de7ececf4fda016bafd8b6aa95faeda
kamalsinghrajput <ksamant366@gmail.com>
2024-07-28 22:18:26 +0530

    css update

--- src/pages/AnimeDetails/animeDetails.jsx
+++ src/pages/AnimeDetails/animeDetails.jsx
@@ -9,6 +9,7 @@ function AnimeDetails() {
   const [animeData, setAnimeData] = useState();
   const { id, category } = useParams();
   console.log(id);
+
   const getData = async () => {
     const response = await fetch(`https://api.jikan.moe/v4/${category}/${id}`);
     const json = await response.json();
@@ -22,40 +23,22 @@ function AnimeDetails() {
   useEffect(() => {
     getData();
   }, [id, category]);
-  const { rank, year, type, source, episodes, status, rating } =
+  const { rank, year, type, source, episodes, status, rating, synopsis} =
     animeData || {};
   const { genres, name } = animeData?.genres || {};
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.page}>
-        <div>
-          <img
-            className={styles.animeImage}
-            src={animeData?.images?.jpg?.image_url}
-            alt=""
-          />
-        </div>
+    <div className={styles.background}>
+      <div className={styles.wrapper}>
         <TitleCard
           title={animeData?.title}
-          desc={animeData?.synopsis}
-          className={styles.titleSection}
+          isHeader= {true}
           tags={[`Rank ${rank}`, year, type, "4K"]}
-          href={animeData?.trailer?.url}
+          image={animeData?.images?.jpg?.image_url}
+          animeData={animeData}
+          desc={synopsis}
         />
-
-        <div className={styles.verticalLine}></div>
-
-        <div className={styles.animestats}>
-          <div>Rank : {rank}</div>
-          <div>Year : {year}</div>
-          <div>Type : {type}</div>
-          <div>Source : {source}</div>
-          <div>Episode : {episodes}</div>
-          <div>Status : {status}</div>
-          <div>Rating : {rating}</div>
-          <div>Genres : {genres}</div>
-          <div className={styles.horizontalLine}></div>
-        </div>
+     
+        
       </div>
     </div>
   );
--- src/utils/TitleCard/TitleCard.jsx
+++ src/utils/TitleCard/TitleCard.jsx
@@ -1,11 +1,40 @@
 import styles from "../../components/Header/header.module.css";
 import GradientButton from "../GradientButton/GradientButton";
-function TitleCard({ title, spotlight, tags, desc, className, href }) {
+function TitleCard({
+  title,
+  spotlight,
+  tags,
+  desc,
+  className,
+  href,
+  isHeader,
+  image,
+  animeData,
+}) {
+  const { genres, name } = animeData?.genres || {};
+  const { rank, year, type, source, episodes, status, rating} =
+    animeData || {};
+  console.log(animeData);
   return (
     <div className={`${styles.content} ${className}`}>
       <div>{spotlight}</div>
       <h2 className={styles.titleName}>{title}</h2>
 
+      {isHeader && (
+        <div className={styles.titleStats}>
+          <img className={styles.animeImage} src={image} alt="" />
+          <div className={styles.animeStats}>
+            <div>Rank : {rank}</div>
+            <div>Year : {year}</div>
+            <div>Type : {type}</div>
+            <div>Source : {source}</div>
+            <div>Episode : {episodes}</div>
+            <div>Status : {status}</div>
+            <div>Rating : {rating}</div>
+            <div>Genres : {genres}</div>
+          </div>
+        </div>
+      )}
       <div className={styles.highlights}>
         {tags.map((item, index) => {
           return (
@@ -15,8 +44,12 @@ function TitleCard({ title, spotlight, tags, desc, className, href }) {
           );
         })}
       </div>
+
+      
       <GradientButton href={href} />
-      <p>{desc}</p>
+      <div className={!isHeader && styles.animeDesc}>
+      <p >{desc}</p>
+      </div>
     </div>
   );
 }
